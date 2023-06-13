ba17ee64391860aa27f3880cdf66c253f1d2021b
unknown <liulie@sky.com>
2023-06-13 10:09:31 +0800

    第二章学习
    第二章学习

---
+++ Source/C_learn/C_learn.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "C_learn.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, C_learn, "C_learn" );
---
+++ Source/C_learn/C_learnGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "C_learnGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class C_LEARN_API AC_learnGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
